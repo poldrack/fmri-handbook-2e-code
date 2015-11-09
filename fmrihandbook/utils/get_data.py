8ae747569a3faf007c1db905e0a004ffbc247dfd
poldrack <poldrack@gmail.com>
2015-11-08 17:35:37 -0800

    moving to BIDS paths

--- fmrihandbook/utils/get_data.py
+++ fmrihandbook/utils/get_data.py
@@ -4,18 +4,20 @@ load data for fmri-handbook-2e notebooks
 
 import os
 
-datadir=os.getenv('FMRIBOOKDATA')
+datadir=os.getenv('FMRIDATADIR')
 assert os.path.exists(datadir)
+
+urlbase='https://s3.amazonaws.com/openfmri/'
 
 def get_data():
     data={}
     # required starting files
-    base_files={'T1':'sub018_t1.nii.gz',
-        'T2':'sub018_t2.nii.gz',
-        'func':'sub00001_ses014_task002_run001_bold.nii.gz',
-        'meanfunc':'sub00001_ses014_task002_run001_bold_mean.nii.gz',
-        'fieldmap-mag':'sub00001_ses014_001_magnitude.nii.gz',
-        'fieldmap-phasediff':'sub00001_ses014_001_phasediff.nii.gz'}
+    base_files={'T1':'ds031/sub-01/ses-018/anat/sub-01_ses-018_run-001_T1w.nii.gz',
+        'T2':'ds031/sub-01/ses-018/anat/sub-01_ses-018_run-001_T2w.nii.gz',
+        'func':'ds031/sub-01/ses-014/func/sub-01_ses-014_task-nback_run-001_bold.nii.gz',
+        'sbref':'ds031/sub-01/ses-014/func/sub-01_ses-014_task-nback_run-001_sbref.nii.gz',
+        'fieldmap-mag':'ds031/sub-01/ses-014/fmap/sub-01_ses-014_magnitude1.nii.gz',
+        'fieldmap-phasediff':'ds031/sub-01/ses-014/fmap/sub-01_ses-014_phasediff.nii.gz'}
 
     missing_base_files=[]
 
@@ -24,6 +26,8 @@ def get_data():
         if os.path.exists(f):
             data[k]=f
         else:
+            print('getting %s'%base_files[k])
+
             missing_base_files.append(base_files[k])
 
 
